d9d35c5cca39a6caa1091f11a21c32d79f0b6d69
RyHiLL <ryhilltechnologies@gmail.com>
2018-03-13 15:40:56 +0530

    Pages

---
+++ ryhill/pages/contact.php
@@ -0,0 +1,67 @@
+<?php
+
+
+$name = ($_GET['name']) ? $_GET['name'] : $_POST['name'];
+$email = ($_GET['email']) ?$_GET['email'] : $_POST['email'];
+$comment = ($_GET['comment']) ?$_GET['comment'] : $_POST['comment'];
+
+
+
+if ($_POST) $post=1;
+
+
+if (!$name) $errors[count($errors)] = 'Please enter your name.';
+if (!$email) $errors[count($errors)] = 'Please enter your email.'; 
+if (!$comment) $errors[count($errors)] = 'Please enter your message.'; 
+
+//if the errors array is empty, send the mail
+if (!$errors) {
+
+	//recipient - replace your email here
+	$to = 'info@ryhilltech.com';	
+	//sender - from the form
+	$from = $name . ' <' . $email . '>';
+	
+	//subject and the html message
+	$subject = 'Message from ' . $name;	
+	$message = 'Name: ' . $name . '<br/><br/>
+		       Email: ' . $email . '<br/><br/>		
+		       Message: ' . nl2br($comment) . '<br/>';
+
+	//send the mail
+	$result = sendmail($to, $subject, $message, $from);
+	
+	//if POST was used, display the message straight away
+	if ($_POST) {
+		if ($result) echo 'Thank you! We have received your message.';
+		else echo 'Sorry, unexpected error. Please try again later';
+		
+	//else if GET was used, return the boolean value so that 
+	//ajax script can react accordingly
+	//1 means success, 0 means failed
+	} else {
+		echo $result;	
+	}
+
+//if the errors array has values
+} else {
+	//display the errors message
+	for ($i=0; $i<count($errors); $i++) echo $errors[$i] . '<br/>';
+	echo '<a href="index.html">Back</a>';
+	exit;
+}
+
+
+//Simple mail function with HTML header
+function sendmail($to, $subject, $message, $from) {
+	$headers = "MIME-Version: 1.0" . "\r\n";
+	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
+	$headers .= 'From: ' . $from . "\r\n";
+	
+	$result = mail($to,$subject,$message,$headers);
+	
+	if ($result) return 1;
+	else return 0;
+}
+
+?>
